78fd54394dce7de2fa319dc20fd42d8cd65afe5e
SafdarAliGit <safdar211@gmail.com>
2023-09-06 23:22:32 +0500

    update

--- ems/ems/doctype/general_election_token/general_election_token.js
+++ ems/ems/doctype/general_election_token/general_election_token.js
@@ -2,48 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('General Election Token', {
-    card_no: function (frm) {
 
-        if (frm.doc.card_no) {
-            frappe.call({
-                method: "ems.ems.doctype.general_election_token.general_election_token.get_active_voter_details",
-                args: {
-                    card_no: frm.doc.card_no
-                },
-                callback: function (response) {
-                    var voter_details = response.message;
-
-                    if (voter_details && voter_details.length > 0) {
-
-
-                        frm.set_value('photo', voter_details[0].photo);
-                        frm.set_value('member_no', voter_details[0].member_no);
-                        frm.set_value('member_name', voter_details[0].member_name);
-                        frm.set_value('father_husband', voter_details[0].father_husband);
-                        frm.set_value('grand_father_name', voter_details[0].grand_father_name);
-                        frm.set_value('khundi', voter_details[0].khundi);
-                        frm.set_value('booth', voter_details[0].booth);
-                        frm.set_value('page', voter_details[0].page);
-                        frm.set_value('vote_no', voter_details[0].vote_no);
-                        frm.set_value('attendance', voter_details[0].attendance);
-
-                        frm.fields_dict['photo_display'].$wrapper.html(
-                            `<img src="${voter_details[0].photo}" alt="Image">`
-                        );
-
-                    } else {
-                        frappe.throw(`No voter details found under card no: ${frm.doc.card_no} .`);
-                    }
-                }
-            });
-        }
-    },
+    // card_no: function (frm) {
+    //
+    //
+    // },
     refresh: function (frm) {
-        // remove standard flex buttons
-        // var divToRemove = document.querySelector('.standard-actions.flex');
-        // if (divToRemove) {
-        //     divToRemove.remove();
-        // }
 
         if (frm.doc.photo) {
             // Image field has a value, display the image
@@ -57,9 +21,8 @@ frappe.ui.form.on('General Election Token', {
         // print button
 
 
-
-            frm.add_custom_button(__('Open Print Format'), function () {
-   if (frm.doc.attendance === 0 && frm.doc.docstatus===0) {
+        frm.add_custom_button(__('Open Print Format'), function () {
+            if (frm.doc.attendance === 0 && frm.doc.docstatus === 0) {
                 // attendance upate
                 frm.doc.attendance = 1;
                 frm.refresh_field('attendance');
@@ -71,10 +34,6 @@ frappe.ui.form.on('General Election Token', {
                     callback: function (response) {
                         if (response && response.message) {
                             frm.reload_doc();
-                            // var divToRemove = document.querySelector('.standard-actions.flex');
-                            // if (divToRemove) {
-                            //     divToRemove.remove();
-                            // }
                             var print_format = 'Ticket'; // Replace with the actual print format name
                             openPrintFormat(frm.docname, print_format);
                             var new_doc = frappe.model.get_new_doc('General Election Token');
@@ -83,11 +42,11 @@ frappe.ui.form.on('General Election Token', {
                         }
                     }
                 });
-    }else {
-       frappe.throw("Ticket Already Printed");
-   }
+            } else {
+                frappe.throw("Ticket Already Printed");
+            }
 
-            });
+        });
 
 // end attendance upate
 
@@ -110,6 +69,106 @@ frappe.ui.form.on('General Election Token', {
         }
 
         // print button end
+
+
+        frm.fields_dict.card_no.$input.on('keydown', function (e) {
+            // Check if the key pressed is Enter (key code 13)
+            if (e.which === 13) {
+                e.preventDefault(); // Prevent the default form submission
+                if (frm.doc.card_no) {
+                    frappe.call({
+                        method: "ems.ems.doctype.general_election_token.general_election_token.get_active_voter_details",
+                        args: {
+                            card_no: frm.doc.card_no
+                        },
+                        callback: function (response) {
+                            var voter_details = response.message;
+
+                            function remove_table() {
+                                var elementToRemove = document.getElementById("responseTable");
+                                if (elementToRemove) {
+                                    elementToRemove.parentNode.removeChild(elementToRemove);
+                                }
+                            }
+
+                            if (voter_details && voter_details.length > 0) {
+
+                                if (voter_details[0].attendance) {
+
+                                    function createTableHTML(data) {
+                                        frm.set_value('photo', data[0].photo)
+                                        var tableHTML = `<table class="table table-bordered" style="font-weight: bold;font-size: 18px;"><tbody>`;
+                                        tableHTML += `
+                                <tr>
+                                    <td style="font-weight: bolder;color:red;">Note: Ticket Already Printed</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>By User</td>
+                                    <td>${voter_details[0].owner}</td>
+                                </tr>
+                                <tr>
+                                    <td>Card No</td>
+                                    <td>${voter_details[0].card_no}</td>
+                                </tr>
+                                <tr>
+                                    <td> At Date Time</td>
+                                    <td>${voter_details[0].date_time}</td>
+                                </tr>`;
+
+
+                                        tableHTML += `</tbody></table>`;
+
+                                        return tableHTML;
+                                    }
+
+
+                                    var formSectionDiv = document.querySelector('.form-section');
+                                    var existingResponseTable = formSectionDiv.nextElementSibling;
+                                    var elementToRemove = document.getElementById("responseTable");
+                                    if (elementToRemove) {
+                                        elementToRemove.parentNode.removeChild(elementToRemove);
+                                    }
+
+                                    var responseTableHTML = createTableHTML(voter_details);
+                                    var tempElement = document.createElement('div');
+                                    tempElement.id = 'responseTable';
+                                    tempElement.innerHTML = responseTableHTML;
+                                    formSectionDiv.insertAdjacentElement('afterend', tempElement);
+
+                                    var new_doc = frappe.model.get_new_doc('General Election Token');
+                                    frappe.set_route('Form', 'General Election Token', new_doc.name);
+                    
+
+                                } else {
+
+                                    frm.set_value('photo', voter_details[0].photo);
+                                    frm.set_value('member_no', voter_details[0].member_no);
+                                    frm.set_value('member_name', voter_details[0].member_name);
+                                    frm.set_value('father_husband', voter_details[0].father_husband);
+                                    frm.set_value('grand_father_name', voter_details[0].grand_father_name);
+                                    frm.set_value('khundi', voter_details[0].khundi);
+                                    frm.set_value('booth', voter_details[0].booth);
+                                    frm.set_value('page', voter_details[0].page);
+                                    frm.set_value('vote_no', voter_details[0].vote_no);
+                                    frm.set_value('attendance', voter_details[0].attendance | 0);
+
+                                    frm.fields_dict['photo_display'].$wrapper.html(
+                                        `<img src="${voter_details[0].photo}" alt="Image">`
+                                    );
+                                    setTimeout(remove_table, 500);
+                                }
+
+
+                            } else {
+                                frappe.throw(`No voter details found under card no: ${frm.doc.card_no} .`);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 
 
--- ems/ems/doctype/general_election_token/general_election_token.py
+++ ems/ems/doctype/general_election_token/general_election_token.py
@@ -12,20 +12,23 @@ class GeneralElectionToken(Document):
 
 @frappe.whitelist()
 def get_active_voter_details(**args):
-    sql_query = """
-        SELECT member_no, member_name, father_husband,
-               grand_father_name, khundi, photo, booth, page, vote_no
-        FROM `tabActive Voter List`
-        WHERE name = %s
-    """
     attendance = """
-        SELECT attendance
-        FROM `tabGeneral Election Token`
-        WHERE card_no = %s
-    """
-
-    results = frappe.db.sql(sql_query, (args.get('card_no'),), as_dict=True)
+            SELECT attendance,date_time,owner,card_no
+            FROM `tabGeneral Election Token`
+            WHERE attendance = 1 AND card_no = %s
+        """
     attendance_result = frappe.db.sql(attendance, (args.get('card_no'),), as_dict=True)
-    if attendance_result:
-        results[0].update(attendance_result[0])
-    return results
+    if not attendance_result:
+        sql_query = """
+            SELECT member_no, member_name, father_husband,
+                   grand_father_name, khundi, photo, booth, page, vote_no
+            FROM `tabActive Voter List`
+            WHERE name = %s
+        """
+        results = frappe.db.sql(sql_query, (args.get('card_no'),), as_dict=True)
+        return results
+    else:
+        return attendance_result
+
+
+
